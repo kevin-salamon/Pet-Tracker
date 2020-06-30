d0541be3bd077efdd36e35a50c5069edadc10133
kevin-salamon <kevinwebdevs@gmail.com>
2020-06-30 13:11:20 -0400

    added construction helper function for main class to be inherited

--- src/Tracker.java
+++ src/Tracker.java
@@ -4,13 +4,23 @@ import java.util.Scanner;
 public class Tracker {
 
     protected String name;
-    protected int age;
+    protected String age;
     protected String color;
 
-    Tracker(String fullName, int years, String colorCoat) {
+    Tracker(String fullName, String years, String colorCoat) {
         this.name = fullName;
         this.age = years;
         this.color = colorCoat;
+    }
+
+    void addPet(String petType) {
+        Scanner key = new Scanner(System.in);
+        System.out.println("What's the name of this " + petType + "?");
+        name = key.nextLine();
+        System.out.println("Great, so how old is " + name + "?");
+        age = key.nextLine();
+        System.out.println("Finally, what color is " + name + "?");
+        color = key.nextLine();
     }
 
     public static void main(String[] args) {
@@ -18,15 +28,13 @@ public class Tracker {
         ArrayList<Cat> cats = new ArrayList<Cat>();
         ArrayList<Dog> dogs = new ArrayList<Dog>();
         ArrayList<Fish> fish = new ArrayList<Fish>();
-
         System.out.println("First, let's get your name: ");
         Scanner key = new Scanner(System.in);
         String petOwnerName = key.nextLine();
         System.out.println("Okay, so your name is " + petOwnerName + ".");
-        System.out.println("Now, let's begin by entering your pets.");
+        System.out.println("Now, let's begin by entering your pets. What pet would you like to add first?");
 
         while (true) {
-            System.out.println("What pet would you like to add?");
             System.out.println("1. Add a dog.");
             System.out.println("2. Add a cat.");
             System.out.println("3. Add a fish.");
@@ -34,13 +42,37 @@ public class Tracker {
             if (key.hasNextInt()) {
                 int petChoice = key.nextInt();
                 if (petChoice == 1) {
-
+                    Dog newDog = new Dog();
+                    newDog.addDog();
+                    dogs.add(newDog);
+                    System.out.println("That's been added! What pet would you like to add next?");
                 } else if (petChoice == 2) {
-
+                    Cat newCat = new Cat();
+                    newCat.addCat();
+                    cats.add(newCat);
+                    System.out.println("That's been added! What pet would you like to add next?");
                 } else if (petChoice == 3) {
-
+                    Fish newFish = new Fish();
+                    newFish.addFish();
+                    fish.add(newFish);
+                    System.out.println("That's been added! What pet would you like to add next?");
                 } else if (petChoice == 4) {
-
+                    System.out.println("Alright - that's all the pets then.");
+                    System.out.println("Okay, so the owner is " + petOwnerName + ". Here are the pets:");
+                    System.out.println("Dogs:");
+                    for(Dog dog : dogs) {
+                        System.out.println();
+                    }
+                    System.out.println("Cats:");
+                    for(Accountant accountant: accountants){
+                        System.out.println("Name: " + accountant.fullName + " | E-mail: " + accountant.email + " | Degree: " + accountant.degree + "| Salary: "+ accountant.fixedSalary);
+                    }
+                    System.out.println("Fish:");
+                    for( Intern intern : interns){
+                        System.out.println("Name: " + intern.fullName + " | E-mail: " + intern.email + " | School: " + intern.school);
+                    }
+                    System.out.println("And that's all the pets! Thanks for using the Pet Tracker today!");
+                    break;
                 } else {
                     System.out.println("Please choose from the available 4 options.");
                 }
